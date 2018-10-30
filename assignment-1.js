7a565f26608cbdfc598f35bf3b27e6ea9ce87bb0
Andrew Nelson <andrew@anothercreative.ca>
2018-10-29 21:52:25 -0700

    Cleaned up assignment-1.js
    Removed excess commenting.
    
    Added extra console.logs

--- assignment-1.js
+++ assignment-1.js
@@ -1,18 +1,4 @@
 //Assignment 1: Basic JavaScript
-//This assignment will test your understanding of loops and data structures in JavaScript.
-
-/* Instructions *
-Create a JavaScript file and name it assignment-1.js. Inside this file:
-
-Create a variable named `numbers` and assign an empty array to it.
-Using a for-loop and the Array push() method, insert the numbers 0 to 9 into the array you named numbers.
-Test that you used the array push method correctly by console logging the first item in the array.
-Console log the last item in the array.
-
-Create another variable named car and assign an empty object to it.
-Give the object a property called colour and assign it the value of "black". */
-
-
 
 // This function inputs a start and end integer and pushes all the integer between into an array and returns the array into the variable invoking it.
 function createIntegerArray(start, end) {
@@ -40,4 +26,7 @@ function createIntegerArray(start, end) {
   let numbers = createIntegerArray(); // Return function result into var
   // Insert array into element by converting it into a string
   stringAnimationLoop('#assignment-1__numbers', numbers, 500);
+
+  console.log(numbers);
+  console.log(car.color);
 })();
